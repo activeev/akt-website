9f11343b754604679b9c199c76f14024a323f496
Laurens Müller-Groh <Laurendus@users.noreply.github.com>
2020-07-20 17:02:42 +0200

    update social-icons.js

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,10 +1,5 @@
 import React from "react"
-import {
-  FaLinkedinIn,
-  FaInstagram,
-  FaFacebookSquare,
-  FaXing,
-} from "react-icons/fa"
+import { FaInstagram, FaFacebookSquare } from "react-icons/fa"
 
 export default [
   {
@@ -14,14 +9,5 @@ export default [
   {
     icon: <FaInstagram />,
     url: "https://www.instagram.com/activebremen",
-  },
-  {
-    icon: <FaLinkedinIn />,
-    url: "https://linkedin.com/company/active-e-v-",
-  },
-  {
-    icon: <FaXing />,
-    url:
-      "https://www.xing.com/companies/activee.v.-studentischeunternehmensberatung",
   },
 ]
